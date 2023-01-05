c8ee8edc7727171c3ecfdc388f0988d7317fdbdb
drensokoli <drensokoli@gmail.com>
2023-01-05 00:01:22 +0000

    Init

---
+++ Containers/ubuntu:focal/file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ky eshte nje file i kopjuar ne ubuntu container permes COPY."
+
+echo $(date)
+
---
+++ Terra_Kafka/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.23.1"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-life"
+    key    = "dren-sokoli/assignment/terraform-dev.tfstate"
+    region = "us-east-1"
+    endpoint = "blob.gjirafa.dev"
+    access_key = "384CXGUJPPWDTJKKLCSX"
+    secret_key = "waqOwHxaH8rP5xoyad5873c8lWxkwj2tqcntjBYz"
+    skip_credentials_validation = true
+    force_path_style            = true
+  }
+
+}
+
+provider "docker" {}
